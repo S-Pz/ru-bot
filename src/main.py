379b05e1747a5e251ddb7350eec12ec3de8f702a
S-Pz <savio_fr@outlook.com>
2024-06-03 23:21:18 -0300

    Adding main file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-import requests
-import schedule 
-import time
 
-from bs4 import BeautifulSoup
+from screpping import obtain_menus
 
 urls:dict = {
 
@@ -14,56 +11,13 @@ urls:dict = {
 
 }
 
-def extract_pdf(url:str, file_name:str):
-    
-    file_path = '../Menus/' + file_name + '.pdf'
-    response = requests.get(url)
-    if (response.status_code == 200):    
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
-    else:
-        print(f"{response.status_code}")
+if __name__ == '__main__':
 
-def obtain_menus(url:str, file_name:str):#file name without extension (pdf)
+    for i in urls:
+       obtain_menus(urls[i], str(i))
+    # obtain_menus(urls['csa_ctan'], 'csa_ctan')
+    # obtain_menus(urls['cco'], 'cco')
+    # obtain_menus(urls['cap'], 'cap')
+    # obtain_menus(urls['cdb'], 'cdb')
+    # obtain_menus(urls['csl'], 'csl')
 
-    response = requests.get(url)
-
-    if (response.status_code == 200):
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find('table')
-        
-        menu_url:str = 'https:' + table.find('a').get('href')
-        
-        extract_pdf(menu_url, file_name)
-
-    else:
-       print(f"{response.status_code}")
-
-
-###1
-# SEPAR OS CÓDIGOS: EXTRAIR DADOS E MONTAR O CSV 
-# SEPARAR OS CÓDIGOS: PARA O A APLICACAO DO BOT
-
-###2
-###OBS 2.1:
-#SALVAR OS ARQUIVOS EM DISCO, USANDO O SQLITE E REALIZAR  SEU CONSUMO
-
-###2.2
-#VER OS ENDPOINTS DA APLICAÇÃO DA BILBLITECA DO BOT
-
-###2.3
-#USAR O RENDER PARA UPAR O BOT, E O CORON JOB AMARADO A UM WEBHOOK DO GITHUB
-
-# def making_csv():
-#     t=0
-#     while True:
-        
-#         print(t)
-#         for i in urls:
-#             print(f'Obtaining {i} menu...')
-#             #obtain_menus(str(urls[i]), str(i))
-
-#         time.sleep(1800)
-#         t+=1
-obtain_menus(urls['csa_ctan'], 'csa_ctan')
---
+++ src/screpping.py
@@ -0,0 +1,57 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def extract_pdf(url:str, file_name:str):
+    
+    file_path = '../Menus/' + file_name + '.pdf'
+    response = requests.get(url)
+    if (response.status_code == 200):    
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+    else:
+        print(f"{response.status_code}")
+
+def obtain_menus(url:str, file_name:str):#file name without extension (pdf)
+    
+    response = requests.get(url, timeout=5)
+    
+    if (response.status_code == 200):
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find('table')
+        
+        menu_url:str = 'https:' + table.find('a').get('href')
+        
+        extract_pdf(menu_url, file_name)
+
+    else:
+       print(f"{response.status_code}")
+
+
+###1
+# SEPAR OS CÓDIGOS: EXTRAIR DADOS E MONTAR O CSV 
+# SEPARAR OS CÓDIGOS: PARA O A APLICACAO DO BOT
+
+###2
+###OBS 2.1:
+#SALVAR OS ARQUIVOS EM DISCO, USANDO O SQLITE E REALIZAR  SEU CONSUMO
+
+###2.2
+#VER OS ENDPOINTS DA APLICAÇÃO DA BILBLITECA DO BOT
+
+###2.3
+#USAR O RENDER PARA UPAR O BOT, E O CORON JOB AMARADO A UM WEBHOOK DO GITHUB
+
+# def making_csv():
+#     t=0
+#     while True:
+        
+#         print(t)
+#         for i in urls:
+#             print(f'Obtaining {i} menu...')
+#             #obtain_menus(str(urls[i]), str(i))
+
+#         time.sleep(1800)
+#         t+=1
