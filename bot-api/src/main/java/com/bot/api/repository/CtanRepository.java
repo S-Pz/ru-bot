79e5f5509c3b1d66230388f329d6d43f883dfa5c
S-Pz <savio_fr@outlook.com>
2026-03-24 17:01:29 -0300

    KANBT-2 Criando o arquivo repository

--- bot-api/src/main/java/com/bot/api/repository/CtanRepository.java
+++ bot-api/src/main/java/com/bot/api/repository/CtanRepository.java
@@ -1,5 +1,12 @@
 package com.bot.api.repository;
 
-public class CtanRepository {
-    
+import com.bot.api.entity.Ctan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface CtanRepository extends JpaRepository<Ctan, Long>{
+    Optional<Ctan> findByData(LocalDate data);
 }
