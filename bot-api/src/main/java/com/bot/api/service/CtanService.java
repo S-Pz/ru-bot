a45d6eef50c24ddfe9d24b540817838bb6762140
S-Pz <savio_fr@outlook.com>
2026-03-25 14:02:51 -0300

    KANBT-2 Criando arquivo Service

--- bot-api/src/main/java/com/bot/api/service/CtanService.java
+++ bot-api/src/main/java/com/bot/api/service/CtanService.java
@@ -1,5 +1,26 @@
 package com.bot.api.service;
 
+import com.bot.api.entity.Ctan;
+import com.bot.api.repository.CtanRepository;
+
+import java.time.LocalDate;
 public class CtanService {
-    
+   
+    private final CtanRepository repository;
+
+    public CtanService(CtanRepository ctanRepository){
+        this.repository = ctanRepository;
+    }
+
+    public Ctan findByDataAndHorario (LocalDate data, String horario){
+        
+        return repository.findByDataAndHorario(data, horario)
+            .orElseThrow(()-> new RuntimeException ("Nenhuma data e horário encontrado."));
+    }
+
+    public Ctan findByData (LocalDate data){
+        
+        return repository.findByData(data)
+            .orElseThrow(()-> new RuntimeException ("Nenhuma data encontrada."));
+    }
 }
